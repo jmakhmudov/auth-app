e8ac244e35d5e379d02054c0bd0bf890d3b728f9
Jaloliddin Makhmudov <makhmudov@tuta.io>
2022-11-20 22:14:46 +0500

    creation of login.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import './styles/App.css'
+import Login from "./components/Login"
 
 function App() {
   
   return (
     <div className="App">
-      App js
+      <Login/>
     </div>
   );
 }
 
-export default App;
+export default App
