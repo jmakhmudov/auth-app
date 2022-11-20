cc65ade3dba5baf489f342d03570444dcda6ed1f
Jaloliddin Makhmudov <makhmudov@tuta.io>
2022-11-20 17:19:09 +0500

    1

---
+++ server/index.js
@@ -0,0 +1,27 @@
+const express = require("express")
+const cors = require("cors")
+const app = express()
+const mySql = require("mysql")
+const PORT = 5000
+
+app.use(cors())
+app.use(express.json())
+
+const db = mySql.createPool({
+  host: "containers-us-west-115.railway.app",
+  user: "root",
+  password: "9t4stuzuBjGVCFGg7wsf",
+  database: "railway",
+  port: "6166"
+})
+
+app.get('/api/insert', (req, res) => {
+  db.query("select * from users", (err, result) => {
+    console.log(err)
+    res.send("insert")
+  })
+})
+
+app.listen(PORT, () => {
+  console.log("runing on port " + PORT)
+})
