2abce17f016ef8795e1cf506063d9599f374a5bf
Jaloliddin Makhmudov <makhmudov@tuta.io>
2022-11-20 17:19:51 +0500

    remove parse

--- client/src/App.js
+++ client/src/App.js
@@ -2,29 +2,8 @@ import logo from './logo.svg';
 import './App.css';
 import Parse from 'parse/dist/parse.min.js';
 
-// Your Parse initialization configuration goes here
-const PARSE_APPLICATION_ID = 'Ts3XlG2kIgBYVPTIlOFThW3FNGc1oeSJW1HTND1q';
-const PARSE_HOST_URL = 'https://parseapi.back4app.com/';
-const PARSE_JAVASCRIPT_KEY = 'WdSRTFzRWSG7FbEkwTgvgAfiPA1Z569W1xE8nlOl';
-Parse.initialize(PARSE_APPLICATION_ID, PARSE_JAVASCRIPT_KEY);
-Parse.serverURL = PARSE_HOST_URL;
-
 function App() {
-  async function addPerson() {
-    try {
-      // create a new Parse Object instance
-      const Person = new Parse.Object('Person');
-      // define the attributes you want for your Object
-      Person.set('name', 'John');
-      Person.set('email', 'john@back4app.com');
-      // save it on Back4App Data Store
-      await Person.save();
-      alert('Person saved!');
-    } catch (error) {
-      console.log('Error saving new person: ', error);
-    }
-  }
-  addPerson()
+  
   return (
     <div className="App">
       <header className="App-header">
